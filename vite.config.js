98ed75262aac80e62c32b5ee2307b6ad8c364051
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-05-22 22:34:48 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,15 @@ import { fullVersionLink, ogImageUrl } from "./src/data"
 
 const headers = [
   `<title>Manassarn Manoonchai - Resumette</title>`,
-  `<meta name="title" content="Manassarn Manoonchai - Resumette" />`,
-  `<meta name="description" content="Interactive & printable résumé with tailored skills fitted in single letter page." />`,
+  `<meta name="title" content="Rachatat Bunpat - Resumette" />`,
+  `<meta name="description" content="Rachatat Bunpat's Resumette" />`,
   `<meta name="viewport" content="width=device-width, initial-scale=1" />`,
   `<meta property="og:url" content=${fullVersionLink} />`,
   `<meta property="og:type" content="website" />`,
-  `<meta property="og:title" content="Manassarn Manoonchai - Resumette" />`,
-  `<meta property="og:description" content="Interactive & printable résumé with tailored skills fitted in single letter page." />`,
+  `<meta property="og:title" content="Rachatat Bunpat - Resumette" />`,
+  `<meta property="og:description" content="Rachatat Bunpat's Resumette" />`,
   `<meta property="og:image" content=${ogImageUrl} />`,
-  `<meta name="twitter:title" content="Manassarn Manoonchai - Resumette" />`,
+  `<meta name="twitter:title" content="Rachatat Bunpat - Resumette" />`,
   `<meta name="twitter:card" content="summary_large_image" />`,
   `<meta name="twitter:image" content=${ogImageUrl} />`,
 ]
