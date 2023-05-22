7638485d110bd62e8471c1f168ea9dd0485794ee
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-05-22 22:40:48 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ const headers = [
   `<meta property="og:type" content="website" />`,
   `<meta property="og:title" content="Rachatat Bunpat - Resumette" />`,
   `<meta property="og:description" content="Rachatat Bunpat's Resumette" />`,
-  `<meta property="og:image" content=${ogImageUrl} />`,
+  `<meta property="og:image" content="https://og-playground.vercel.app/?share=zZTBbtQwEIZfxTJC6UqOSELabSNAQgWhXlskLlyceJx1m9iRPWG7rPbdGaeb7bJcOIDgECf-_8l4vrHsLW-cAl7xN8p8-2oZC7jp4O12G78ZUyYMndxULNEdPCbiSV2BaVdIYp5lL2dxbRSuTjTZmdbeIPSBjAYsgp-t-zGg0ZtrR6KNuX6243IfjIcGjbPRdd3Y29mtZfPQejdaddPLFsjvjAXp09ZLZSjRGTpWO0TXC_ZC1brUmj601rnOF4c1aO07851-v8j2UgdIRdwNsjG2rVhaHIV-2VMvszka4RHfR8ST8ne7OL6Lw9zW08b-DsNVpqAVzLf1WSZYXpSCFWW2OChF8ZqG_GJxIDrOet2ZgZLGGp_tJF1D_WAwfY5LGwpMfomkhjsfVS9tGKSngmbzCI-xW9msJEpMAruFMPYwUb8i7D_Jnxe5YGVkzop9A4rzc8Gmvlz-swb8FdYJbLmMz4xaXkVy2m3a_v-BdX5zwd0QT2jg1ZZPNwCvLul88KcrgldlnCiox5ZXWnYBBIfe3ZvPmyHeOrieZpQnnrCPfQ2KV-hH2AmOsqaIT_sOBb77AQ" />`,
   `<meta name="twitter:title" content="Rachatat Bunpat - Resumette" />`,
   `<meta name="twitter:card" content="summary_large_image" />`,
   `<meta name="twitter:image" content=${ogImageUrl} />`,
